611fa1eaab24026ab5b0e40fa3217519e8651582
hillphang <26hillary.phang@lwhs.org>
2025-01-09 17:52:16 +0000

    submitting first draft

--- project/mySketch.js
+++ project/mySketch.js
@@ -1,8 +1,16 @@
+
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(700, 700);
 }
 
 function draw() {
-  background(200);
-  
+  background(100,100,100);
+    background(200);
+    fill ("white")  
+    ellipse(90, 50, 80, 40);
+    describe('A white ellipse on a gray canvas.');
+    circle(90, 50, 25);
+    circle(105, 55, 10);
 }
+
