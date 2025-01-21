0a643256f58a092a08e9fb01dbe72bda89993227
hillphang <26hillary.phang@lwhs.org>
2025-01-21 20:04:07 +0000

    submitting next draft

--- project/functionsExample.js
+++ project/functionsExample.js
@@ -1,7 +1,10 @@
+
+ellipse(x-5,y,30,30)
 function setup() {
     createCanvas(600, 600);
    background("pink")
     eyes(200,200);
+    eyes(100,300)
    
  }
  
@@ -9,19 +12,24 @@ function setup() {
  }
  
  function eyes(x,y) {
-   //left eye
-     fill("white")
-    ellipse(x, y, 80, 50);
-     fill("black")
-     ellipse(x-5,y,30,30)
-    fill("white")
-     rect(x-10, y-10, 10, 10);
+let color = "red";
+let colors = ["red", "green", "blue"];
+fill(color)
+ellipse(x-5,y,30,30)
+fill(colors[1])
+ //left eye
+  fill("white")
+  ellipse(x, y, 80, 50);
+  fill("black")
+  ellipse(x-5,y,30,30)
+  fill("white")
+  rect(x-10, y-10, 10, 10);
    
-   //right eye
-     fill("white")
-    ellipse(x+200, y, 80, 50);
-     fill("black")
-     ellipse(x+195,y,30,30)
-    fill("white")
-     rect(x+190, y-10, 10, 10);
+ //right eye
+  fill("white")
+  ellipse(x+200, y, 80, 50);
+  fill("black")
+  ellipse(x+195,y,30,30)
+  fill("white")
+  rect(x+190, y-10, 10, 10);
  }
--- project/mySketch.js
+++ project/mySketch.js
@@ -1,16 +1,37 @@
 
 
+ let xValue = 350;
+
 function setup() {
   createCanvas(700, 700);
 }
 
 function draw() {
-  background(100,100,100);
-    background(200);
-    fill ("white")  
-    ellipse(90, 50, 80, 40);
-    describe('A white ellipse on a gray canvas.');
-    circle(90, 50, 25);
-    circle(105, 55, 10);
+  background("pink");
+  eye()
+  egg()
 }
+   
+  
+   
+   function eye() {
+      fill ("white")  
+    ellipse(90, 50, 80, 40);
+    circle(90, 50, 25); 
+    fill(77, 51, 18)
+    circle(90, 51, 10);
+    
+    fill ("white") 
+    ellipse(200, 50, 80, 40);
+    circle(200, 50, 25);
+    fill(77, 51, 18)
+    circle(200, 51, 10);
+   }
+   
+   function egg () {
+    fill(200,10,0);
+    ellipse(xValue,200,50,70);
+    xValue = xValue - 50;
+   }
+
 
